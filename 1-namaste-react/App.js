39c217e1189eaa310a87b685922bacc1ed25a350
KenKot <kennykotten@gmail.com>
2024-02-17 23:50:37 -0800

    ep2

--- 1-namaste-react/App.js
+++ 1-namaste-react/App.js
@@ -1,23 +1,20 @@
-// const heading = React.createElement(
-//   "h1", //props
-//   {id: "heading"},
-//   "Hello world from react" //children
-// );
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement(
   "div", //what type of tag?
-  {id: "parent"}, //what attributes?
+  { id: "parent" }, //what attributes?
   React.createElement(
     //the children - this can be converted to an array for multiple children
     "div",
-    {id: "child"},
+    { id: "child" },
     [
       React.createElement("h1", {}, "I am an h1 tag"),
-      React.createElement("h1", {}, "I am an h1 tag also"),
+      React.createElement("h1", {}, "I am an h1 tag also!"),
     ]
   )
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
+
 root.render(parent);
