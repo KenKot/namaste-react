991811a1a0aba787ef55cdb554b82a6b9c5c30fd
KenKot <kennykotten@gmail.com>
2024-02-19 15:36:28 -0800

    ep 4 part 1

--- 1-namaste-react/App.js
+++ 1-namaste-react/App.js
@@ -1,22 +1,71 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React Element
-//tag       attributes   children(can be an array)
-// const heading = React.createElement("h1", {id: "heading"}, "Namaste React");
-
-const Parent = () => {
+const Header = () => {
   return (
-    <div>
-      <h1>Parent stuff</h1>
-      <Child />
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://images-platform.99static.com/A_Ax0GQuo_NHI0Y7XZHmFtGfBDY=/0x0:1000x1000/500x500/top/smart/99designs-contests-attachments/126/126252/attachment_126252018"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
   );
 };
 
-const Child = () => {
-  return <h3>child stuff</h3>;
+const styleCard = {
+  backgroundColor: "#f0f0f0",
+};
+
+// de-structuring props:
+// const RestaurantCard = ({resName}) => {
+const RestaurantCard = (props) => {
+  // inline CSS version 2
+  // <div className="res-card" style={{backgroundColor: "blue"}}>
+  return (
+    <div className="res-card" style={styleCard}>
+      <img
+        className="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1674029845/PC_Creative%20refresh/3D_bau/banners_new/Burger.png"
+        alt="res-logo"
+      />
+      <h3>{props.resName}</h3>
+      <h4>Birayna, North Indian, Asian</h4>
+      <h4>4.4 stars</h4>
+      <h4>30 minutes</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">search</div>
+      <div className="res-container">
+        <RestaurantCard resName="Meghana Foods" />
+        <RestaurantCard resName="KFC" />
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Parent />);
+root.render(<AppLayout />);
