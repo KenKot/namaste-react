f9170e9a23d8a40e9a405a9a2d657acd387502fb
KenKot <kennykotten@gmail.com>
2024-02-22 16:23:47 -0800

    ep6

--- 1-namaste-react/src/components/Body.js
+++ 1-namaste-react/src/components/Body.js
@@ -1,46 +1,75 @@
 import RestaurantCard from "./RestaurantCard";
 import mockData from "../utils/mockData";
+import Shimmer from "./Shimmer";
 
 import { useState, useEffect } from "react";
 
 const Body = () => {
-  // const [data, setData] = useState(mockData);   OLD
+  console.log("Body() triggered");
   const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+
+  const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
-    fetchData();
     console.log("useEffect fired");
+    fetchData();
   }, []);
 
   const fetchData = async () => {
     const data = await fetch(
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+      // "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
 
-    console.log(json);
+    console.log("fetchData fired: ", json);
 
     // setData(json?.data?.cards[4]?.data?.data?.cards);
-
     setData(
-      json?.data?.cards[4].card.card.gridElements.infoWithStyle.restaurants
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+
+    setFilteredData(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
 
     console.log(
-      "~~~",
+      "response data: ",
       json?.data?.cards[4].card.card.gridElements.infoWithStyle.restaurants
     );
     // setData(json.data.cards[2].data.data.cards);
   };
 
-  return (
+  return !data.length ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              const filteredRestaurants = data.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+              setFilteredData(filteredRestaurants);
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
             console.log("filter button fired");
-            // setData((prev) => prev.filter((res) => res.data.avgRating > 4));
             setData((prev) => prev.filter((res) => res.info.avgRating > 4.4));
           }}
         >
@@ -48,8 +77,7 @@ const Body = () => {
         </button>
       </div>
       <div className="res-container">
-        {data.map((res, idx) => (
-          // <RestaurantCard res={res} key={res?.data?.id} />
+        {filteredData.map((res, idx) => (
           <RestaurantCard res={res} key={res.info.id} />
         ))}
       </div>
--- 1-namaste-react/src/components/Header.js
+++ 1-namaste-react/src/components/Header.js
@@ -1,6 +1,10 @@
-import {LOGO_URL} from "../utils/constants";
+import { useState } from "react";
+import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
+  console.log("Header() ran");
+  const [btnName, setBtnName] = useState("Login");
+
   return (
     <div className="header">
       <div className="logo-container">
@@ -12,6 +16,14 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              btnName === "Login" ? setBtnName("Logout") : setBtnName("Login");
+            }}
+          >
+            {btnName}
+          </button>
         </ul>
       </div>
     </div>
