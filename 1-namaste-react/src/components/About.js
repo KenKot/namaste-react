28ce1f07a8001286fe11c3d7f7428e553b8a23a5
KenKot <kennykotten@gmail.com>
2024-02-25 15:21:13 -0800

    ep8 part 1

--- 1-namaste-react/src/components/About.js
+++ 1-namaste-react/src/components/About.js
@@ -1,8 +1,19 @@
+import User from "./User";
+import UserClass from "./UserClass";
 const About = () => {
   return (
     <div>
       <h1>About</h1>
       <h2>This is namaste react</h2>
+
+      <h3>Functional Component:</h3>
+      <User name={"Jimmy"} location={"France"} email={"Jimmy@gmail.com"} />
+      <h3>Class Based Component:</h3>
+      <UserClass
+        name={"Bobby"}
+        location={"England"}
+        email={"Bobby@gmail.com"}
+      />
     </div>
   );
 };
